[python] fix: gradient calculation (#24)
- silu fused_kernel 사용할 때, gradient가 제대로 계산되지않던 문제를 수정

resolved: #23

Co-authored-by: seopbo <seopbo0130@gmail.com>

megatron/model/fused_bias_silu.py
@@ -37,7 +37,8 @@ def bias_silu_back(g, bias, y):
     x = bias + y
     silu_out = x * torch.sigmoid(x)
     sigmoid_out = torch.sigmoid(x)
-    return sigmoid_out + silu_out * (1 - sigmoid_out)
+    ff = sigmoid_out + silu_out * (1 - sigmoid_out)
+    return g * ff
     
 
 class SiLUFunction(torch.autograd.Function):

megatron/model/fused_silu.py
@@ -35,7 +35,8 @@ def silu(x):
 def silu_back(g, x):
     silu_out = x * torch.sigmoid(x)
     sigmoid_out = torch.sigmoid(x)
-    return sigmoid_out + silu_out * (1 - sigmoid_out)
+    ff = sigmoid_out + silu_out * (1 - sigmoid_out)
+    return g * ff
 
 class SiLUFunction(torch.autograd.Function):
     @staticmethod

megatron/model/transformer.py
@@ -245,16 +245,17 @@ class ParallelMLPSwiGLU(MegatronModule):
         # [s, b, ]
         gate_parallel, gate_bias_parallel = self.gate_proj(hidden_states)
 
-        if gate_bias_parallel is not None:
-            if self.bias_silu_fusion:
-                gate_parallel = bias_silu_impl(gate_parallel, gate_bias_parallel)
+        with torch.enable_grad():
+            if gate_bias_parallel is not None:
+                if self.bias_silu_fusion:
+                    gate_parallel = bias_silu_impl(gate_parallel, gate_bias_parallel)
+                else:
+                    gate_parallel = self.activation_func(gate_parallel + gate_bias_parallel)
             else:
-                gate_parallel = self.activation_func(gate_parallel + gate_bias_parallel)
-        else:
-            if self.silu_fusion:
-                gate_parallel = silu_impl(gate_parallel)
-            else:
-                gate_parallel = self.activation_func(gate_parallel)
+                if self.silu_fusion:
+                    gate_parallel = silu_impl(gate_parallel)
+                else:
+                    gate_parallel = self.activation_func(gate_parallel)
         
         # [s, b, ]
         up_parallel, up_bias_parallel = self.up_proj(hidden_states)